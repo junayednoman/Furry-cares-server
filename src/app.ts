a2613f827882b3715e1d1452dd8bb0f3285f330e
Junayed Noman <junayednoman05@gmail.com>
2024-10-11 08:53:36 +0600

    feat: new cors origin added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
- 
- 
+
+
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
@@ -10,7 +10,7 @@ const app: Application = express()
 app.use(express.json())
 
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000', 'https://furry-cares.vercel.app'],
   credentials: true
 }))
 
